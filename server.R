c4c13cd8021a5fe190a21f54e2d161749345e59f
Melissa <mrrineer@ncsu.edu>
2019-07-30 14:42:01 -0400

    push

--- server.R
+++ server.R
@@ -264,7 +264,7 @@ shinyServer(function(input, output, session) {
     data.frame(`Age-adjusted Death Rate`=input$ageRate, Deaths= input$numDeaths)
   })
       #fit model
-      knnFit <- train(`Cause Name` ~ `Age-adjusted Death Rate`+Deaths, data = deathDataTrain, method = "knn",
+      knnFit <- train(`Cause Name` ~ `Age-adjusted Death Rate`+Deaths, data = deathData, method = "knn",
                        trControl = trainControl(method = "repeatedcv", number = 10, repeats = 5),
                        preProcess = c("center", "scale"))
   output$knn<-renderPrint({  
